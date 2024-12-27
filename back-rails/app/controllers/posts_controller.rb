0fb691298f6d0baa298812c296d74524ab52c21c
Caio Gullo <caioinacio19@gmail.com>
2024-12-26 23:05:05 -0300

    feat: atualizando lógica da controller para lidar com json

--- back-rails/app/controllers/posts_controller.rb
+++ back-rails/app/controllers/posts_controller.rb
@@ -1,27 +1,30 @@
 class PostsController < ApplicationController
+    skip_before_action :verify_authenticity_token, only: [:create]
 
-#exibir todos os posts
+    # Exibir todos os posts
     def index
-        if params[:query].present?
-            @posts = Post.where("title LIKE ? OR content LIKE ?", "%#{params[:query]}%", "%#{params[:query]}%")
-        else
-            @posts = Post.all
-        end
+      if params[:query].present?
+        @posts = Post.where("title LIKE ? OR content LIKE ?", "%#{params[:query]}%", "%#{params[:query]}%")
+      else
+        @posts = Post.all
+      end
+      render json: @posts 
     end
-
+  
     def create
-        @post = Post.new(post_params)
-        if @post.save
-            redirect_to posts_path, notice: "Post criado com sucesso!"
-        else
-            render :new, alert: "Erro ao criar post!"
-        end
-
+      @post = Post.new(post_params)
+      if @post.save
+        render json: @post, status: :created 
+      else
+        render json: { errors: @post.errors.full_messages }, status: :unprocessable_entity 
+      end
     end
-
+    
+  
     private
-
+  
     def post_params
-        params.require(:post).permit(:title, :content)
-        end
+      params.require(:post).permit(:title, :content)
     end
+  end
+  
