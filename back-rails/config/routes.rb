1622a3fedf893f62fcd2c025d9702ce6e1fc3079
Caio Gullo <caioinacio19@gmail.com>
2024-12-27 09:07:10 -0300

    feat: adicionando rotas de login e cadastro

--- back-rails/config/routes.rb
+++ back-rails/config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
   resources :posts, only: [:index, :create]
   root "posts#index"
 
+  # Rotas para Usuários
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
+
+  # Rotas para Sessões
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
 end
