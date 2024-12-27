87141b26328cd14c3e9d9781b0fec50677a949bc
Caio Gullo <caioinacio19@gmail.com>
2024-12-27 18:48:30 -0300

    fix: configurar CORS para permitir requisições do frontend

--- back-rails/config/initializers/cors.rb
+++ back-rails/config/initializers/cors.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8080' # URL do seu frontend
+    origins 'https://my-rails-app.vercel.app' 
     resource '*',
              headers: :any,
-             methods: [:get, :post, :put, :patch, :delete, :options],
-             credentials: true # Permite envio de cookies e autenticação
+             methods: [:get, :post, :put, :patch, :delete, :options, :head],
+             credentials: true 
   end
 end
