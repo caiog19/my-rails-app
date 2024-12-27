ff4c7aac2a1be6aec46e5c6369c57b46164e5930
Caio Gullo <caioinacio19@gmail.com>
2024-12-26 23:11:45 -0300

    feat: configura axios para incluir o token CSRF

--- front-vue/src/services/api.js
+++ front-vue/src/services/api.js
@@ -1,9 +1,13 @@
-import axios from'axios';
+import axios from 'axios';
 
 const api = axios.create({
-
-    baseURL: 'http://localhost:3000/'
-
+  baseURL: 'http://localhost:3000', 
+  withCredentials: true, 
 });
+
+const csrfToken = document.querySelector('meta[name="csrf-token"]')?.getAttribute('content');
+if (csrfToken) {
+  api.defaults.headers.common['X-CSRF-Token'] = csrfToken;
+}
 
 export default api;
