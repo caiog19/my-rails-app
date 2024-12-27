462a1903bba364a3fe5d4f354582bf3994855fff
Caio Gullo <caioinacio19@gmail.com>
2024-12-26 22:39:47 -0300

    feat: altera appvue para criar e postar

--- front-vue/src/App.vue
+++ front-vue/src/App.vue
@@ -7,6 +7,19 @@
         <p>{{ post.content }}</p>
       </li>
     </ul>
+
+    <h2>Criar novo Post</h2>
+    <form @submit.prevent="createPost">
+      <div>
+        <label for="title">Título:</label>
+        <input v-model="newPost.title" id="title" />
+      </div>
+      <div>
+        <label for="content">Conteúdo:</label>
+        <textarea v-model="newPost.content" id="content"></textarea>
+      </div>
+      <button type="submit">Criar Post</button>
+    </form>
   </div>
 </template>
 
@@ -17,15 +30,35 @@ export default {
   data() {
     return {
       posts: [],
+      newPost: {
+        title: '',
+        content: '',
+      },
     };
   },
   async created() {
-    try {
-      const response = await api.get('/posts');
-      this.posts = response.data;
-    } catch (error) {
-      console.error('Erro ao buscar posts:', error);
-    }
+    await this.fetchPosts();
+  },
+  methods: {
+    async fetchPosts() {
+      try {
+        const response = await api.get('/posts');
+        this.posts = response.data;
+      } catch (error) {
+        console.error('Erro ao buscar posts:', error);
+      }
+    },
+    async createPost() {
+      try {
+        const response = await api.post('/posts', {
+          post: this.newPost,
+        });
+        this.posts.push(response.data);
+        this.newPost = { title: '', content: '' }; // Reseta o formulário
+      } catch (error) {
+        console.error('Erro ao criar post:', error);
+      }
+    },
   },
 };
 </script>
