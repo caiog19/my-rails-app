28580b7f379f2108bab0e4000fd3795ee0dedd1e
Caio Gullo <caioinacio19@gmail.com>
2024-12-27 12:18:51 -0300

    feat: criando campos de reset_password na model de user

--- back-rails/app/models/user.rb
+++ back-rails/app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
-
   has_secure_password 
   validates :email, presence: true, uniqueness: true
   has_many :posts
+  has_many :comments
+
+  def generate_reset_token!
+    update(
+      reset_password_token: SecureRandom.hex(10),
+      reset_password_sent_at: Time.now
+    )
+  end
 end
