4295911685ba450324632b22b704f4b26bb12750
Caio Gullo <caioinacio19@gmail.com>
2024-12-26 23:08:33 -0300

    feat: configurando CORS

--- back-rails/config/initializers/cors.rb
+++ back-rails/config/initializers/cors.rb
@@ -1,7 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'http://localhost:8080' 
-      resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
-    end
+  allow do
+    origins 'http://localhost:8080' # URL do seu frontend
+    resource '*',
+             headers: :any,
+             methods: [:get, :post, :put, :patch, :delete, :options],
+             credentials: true # Permite envio de cookies e autenticação
   end
-  
+end
