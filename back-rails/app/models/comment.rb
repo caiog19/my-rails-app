0516c3bd09026ed3c8007b6b423415a81a4d7249
Caio Gullo <caioinacio19@gmail.com>
2024-12-28 14:32:03 -0300

    Implementar funcionalidade de comentários no back-end

--- back-rails/app/models/comment.rb
+++ back-rails/app/models/comment.rb
@@ -2,5 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user, optional: true
 
-  validates :content, presence: true
+  validates :content, presence: { message: "não pode estar vazio" }
+
+  def author_name
+    user ? user.full_name : "Anônimo"
+  end
 end
