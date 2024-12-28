f53fd16f04be40c83b69c4d32936ccb6762eb6b7
Caio Gullo <caioinacio19@gmail.com>
2024-12-28 08:31:27 -0300

    fix: corrige a url do email

--- back-rails/app/mailers/user_mailer.rb
+++ back-rails/app/mailers/user_mailer.rb
@@ -1,9 +1,7 @@
 class UserMailer < ApplicationMailer
-  def password_reset(reset_token)
+  def password_reset(reset_link)
     @user = params[:user]
-    frontend_url = Rails.application.credentials.frontend[:url].chomp('/')
-    @reset_link = "#{frontend_url}/password_resets/#{reset_token}"
-
+    @reset_link = reset_link
     mail(to: @user.email, subject: 'Instruções para recuperação de senha') do |format|
       format.html { render 'password_reset' }
       format.text { render 'password_reset' }
