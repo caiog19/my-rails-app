1d26860b533ea983db3b9c299afbaa5822e0b524
Caio Gullo <caioinacio19@gmail.com>
2024-12-28 10:50:34 -0300

    feat: implementando validação no cadastro pelo back

--- back-rails/app/controllers/users_controller.rb
+++ back-rails/app/controllers/users_controller.rb
@@ -3,11 +3,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+
     if @user.save
-      token = JsonWebToken.encode({ user_id: @user.id }) 
+      token = JsonWebToken.encode({ user_id: @user.id })
       render json: { token: token, message: "Usuário cadastrado com sucesso!" }, status: :created
     else
-      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: format_errors(@user.errors) }, status: :unprocessable_entity
     end
   end
 
@@ -15,5 +16,16 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation, :full_name)
+  end
+
+  def format_errors(errors)
+    errors.details.map do |field, details|
+      details.map do |error_detail|
+        {
+          field: field,
+          message: errors.full_message(field, error_detail[:error])
+        }
+      end
+    end.flatten
   end
 end
--- back-rails/app/models/user.rb
+++ back-rails/app/models/user.rb
@@ -1,13 +1,34 @@
 class User < ApplicationRecord
   has_secure_password 
-  validates :email, presence: true, uniqueness: true
-  has_many :posts
-  has_many :comments
 
+  validates :email,
+            presence: { message: "não pode estar em branco" },
+            uniqueness: { message: "já está em uso" },
+            format: { with: URI::MailTo::EMAIL_REGEXP, message: "não é válido" }
+
+  validates :full_name,
+            presence: { message: "não pode estar em branco" },
+            length: { maximum: 100, message: "deve ter no máximo 100 caracteres" }
+
+  validates :password,
+            presence: { message: "não pode estar em branco" },
+            confirmation: { message: "não coincide com a confirmação" },
+            length: { minimum: 8, message: "deve ter no mínimo 8 caracteres" }
+
+  validates :password_confirmation,
+            presence: { message: "é obrigatório" }
+
+  before_save :capitalize_full_name
   def generate_reset_token!
     update(
       reset_password_token: SecureRandom.hex(10),
       reset_password_sent_at: Time.now
     )
+  end
+
+  private
+
+  def capitalize_full_name
+    self.full_name = full_name.split.map(&:capitalize).join(' ') if full_name.present?
   end
 end
