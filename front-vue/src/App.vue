a3796d54a16cad93cd5c47a8048139a9116c9c09
Caio Gullo <caioinacio19@gmail.com>
2025-01-02 11:14:33 -0300

    fix: remove botoes de login e cadastro quando logado

--- front-vue/src/App.vue
+++ front-vue/src/App.vue
@@ -1,36 +1,44 @@
 <template>
   <div id="app">
     <nav class="navbar">
-      <span class="navbar-title">Navega Mainô</span>
+      <div class="navbar-left">
+        <span class="navbar-title">Navega Mainô</span>
+      </div>
       
-      <ul class="navbar-list">
-        <li class="navbar-item">
-          <router-link class="navbar-link" to="/">Home</router-link>
-        </li>
-        <li class="navbar-item">
-          <router-link class="navbar-link" to="/login">Login</router-link>
-        </li>
-        <li class="navbar-item">
-          <router-link class="navbar-link" to="/register">Cadastrar</router-link>
-        </li>
-        <li class="navbar-item" v-if="eventBus.isAuthenticated">
-          <router-link class="navbar-link" to="/meus-posts">Meus Posts</router-link>
-        </li>
-      </ul>
+      <div class="navbar-center">
+        <ul class="navbar-list">
+          <li class="navbar-item">
+            <router-link class="navbar-link" to="/">Home</router-link>
+          </li>
+          <li class="navbar-item" v-if="!eventBus.isAuthenticated">
+            <router-link class="navbar-link" to="/login">Login</router-link>
+          </li>
+          <li class="navbar-item" v-if="!eventBus.isAuthenticated">
+            <router-link class="navbar-link" to="/register">Cadastrar</router-link>
+          </li>
+          <li class="navbar-item" v-if="eventBus.isAuthenticated">
+            <router-link class="navbar-link" to="/meus-posts">Meus Posts</router-link>
+          </li>
+        </ul>
+      </div>
       
-      <button
-        v-if="eventBus.isAuthenticated"
-        @click="logout"
-        class="logout-button"
-        aria-label="Logout"
-      >
-        Sair
-      </button>
+      <div class="navbar-right">
+        <button
+          v-if="eventBus.isAuthenticated"
+          @click="logout"
+          class="logout-button"
+          aria-label="Logout"
+        >
+          Sair
+        </button>
+      </div>
     </nav>
     
     <router-view />
   </div>
 </template>
+
+
 
 <script>
 import { eventBus } from './eventBus';
